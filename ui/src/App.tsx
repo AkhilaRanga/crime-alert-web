5cf2279ffa2c333f8b667f1967f4e25538eed7ed
Akhila Ranga <akhilandeshwari.ranga@gmail.com>
2022-09-25 21:45:41 -0400

    SN-21: Created ui for Registration modal

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Button } from "@material-ui/core";
+import Registration from "./pages/Registration/Registration";
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World!</p>
-      </header>
-    </div>
+    <>
+      <Button onClick={handleOpen} variant="contained" color="primary">
+        Create a new account
+      </Button>
+      <Registration open={open} setOpen={setOpen} />
+    </>
   );
 }
 
