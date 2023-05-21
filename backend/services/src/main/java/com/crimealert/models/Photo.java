2e9cdf11db9bd5e5d435719417c7a3fd084c6af4
Bhanu Preeti Anand <56186569+preeti297@users.noreply.github.com>
2023-05-21 11:34:57 -0600

    Create Post, Upload Photos and Videos API (#9)

--- backend/services/src/main/java/com/crimealert/models/Photo.java
+++ backend/services/src/main/java/com/crimealert/models/Photo.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.bson.types.Binary;
+import org.bson.types.ObjectId;
 
 public class Photo {
 	private String _id;
